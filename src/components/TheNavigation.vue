af6c77db16ba798ec0065fefde2f232a855ad971
Liam Moynihan <liammoyn@gmail.com>
2020-05-24 12:53:13 -0400

    Some initial styling for header

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="header">
     <div class="title">
-      <router-link to="/" tag="h3">
+      <router-link to="/" tag="div">
         <img class="header-logo" src="https://cdn.firespring.com/images/d8b7f14f-5a80-445d-96e7-49cfd18526f7.png" height="75px"/>
       </router-link>
     </div>
-    <access-control :roles="[USER[ROLE.GP], USER[ROLE.PF]]">
-      <div class="navlinks">
-          <router-link to="/events" tag="button">Events</router-link>
-          <router-link to="/my-events" tag="button">Registrations</router-link>
-          <router-link to="/checkout" tag="button">Checkout</router-link>
-          <router-link to="/profile" tag="button">Profile</router-link>
-          <button v-on:click="logout">Log Out</button>
+    <access-control _class="navlinks" :roles="[USER[ROLE.GP], USER[ROLE.PF], USER[ROLE.ADMIN]]">
+      <access-control _class="header-btn" :roles="[USER[ROLE.GP], USER[ROLE.PF]]">
+        <router-link to="/events" tag="button">My Events</router-link>
+      </access-control>
+      <access-control _class="header-btn" :roles="[USER[ROLE.ADMIN]]">
+        <router-link to="/create-event" tag="button">Create Event</router-link>
+      </access-control>
+      <div class="header-btn">
+        <router-link to="/upcoming-events" tag="button">Upcoming Events</router-link>
       </div>
-    </access-control>
-    <access-control :roles="[USER[ROLE.ADMIN]]">
-      <div class="navlinks">
-          <router-link to="/events" tag="button">Events</router-link>
-          <router-link to="/my-events" tag="button">Registrations</router-link>
-          <router-link to="/checkout" tag="button">Checkout</router-link>
-          <router-link to="/create-event" tag="button">Create Event</router-link>
-          <router-link to="/profile" tag="button">Profile</router-link>
-          <button v-on:click="logout">Log Out</button>
+      <div class="header-btn">
+        <router-link to="/profile" tag="button">Home</router-link>
+      </div>
+      <div class="header-btn logout">
+        <button v-on:click="logout">Log Out</button>
       </div>
     </access-control>
   </div>
@@ -66,39 +64,47 @@ export default {
 </script>
 
 <style lang="less" scoped>
-@import '../../assets/global-classes.less';
+  @import '../../assets/global-classes.less';
 
-.header {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  min-height: 8vh;
-  background-color: @header-color;
-  background: linear-gradient(90deg, rgba(61,168,72,0.92) 0%, rgba(151,244,86,0.83) 100%);
-  font-family: var(--main-font);
-  .title {
-    text-transform: uppercase;
-    letter-spacing: 6px;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1em;
+    background: linear-gradient(90deg, rgba(61,168,72,0.92) 0%, rgba(151,244,86,0.83) 100%);
+
+    .title {
+      text-transform: uppercase;
+      letter-spacing: 6px;
+      margin-top: 6px;
+    }
   }
+
+
   .navlinks {
     display: flex;
     justify-content: space-between;
-    white-space: nowrap;
-    button {
-      display: block;
-      width: 100%;
-      height: 5em;
-      letter-spacing: 1px;
-      font-weight: bold;
-      background: none;
-      text-decoration: none;
-      border: none;
-      outline: none;
-      font-family: var(--main-font);
-    }
-    button:hover {
-      color: @button-hover-color;
+    align-items: baseline;
+
+    .header-btn {
+      margin: 0 1em;
+      box-sizing: border-box;
+
+      button {
+        border: none;
+        outline: none;
+        background: transparent;
+        width: 100%;
+        height: 100%;
+
+        cursor: pointer;
+        font-size: 1rem;
+        padding: 1rem;
+      }
+
+      button:hover {
+        color: white;
+      }
     }
   }
-}
 </style>

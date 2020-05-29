27e23967cf6de134dd1f0350fcf160cc8415df8a
Liam Moynihan <liammoyn@gmail.com>
2020-05-29 16:41:37 -0400

    Small fix

--- src/views/ChangePasswordlView.vue
+++ src/views/ChangePasswordlView.vue
@@ -70,7 +70,7 @@ export default {
     return {
       currentPassword: '',
       newPassword: ['', ''],
-      submitErrors: ['', ''],
+      submitErrors: ['', '', ''],
     };
   },
   methods: {

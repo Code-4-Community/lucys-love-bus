da0f5a6d4d6aa34d93e1f91a192408963666f930
Zach O'Brien <zachobrien99@gmail.com>
2020-08-10 23:48:37 -0400

    announcements store module complete

---
+++ src/store/modules/announcements.js
@@ -0,0 +1,46 @@
+import api from '../../api/api';
+
+export default {
+  namespaced: true,
+  state: {
+    announcements: [],
+  },
+  getters: {
+    getSitewideAnnouncements(state) {
+      return state.announcements.filter(
+        ann => !("event_id" in ann));
+    },
+    getEventAnnouncements(state, eventId) {
+      return state.announcements.filter(
+        ann => ("event_id" in ann) && ann['event_id'] === eventId);
+    }
+  },
+  mutations: {
+    loadAnnouncements(state, { announcements }) {
+      state.announcements.push(announcements);
+    },
+    removeAnnouncement(state, payload) {
+      state.announcements =
+        state
+        .announcements
+        .filter(ann => ann.id !== payload.announcementId);
+    },
+    clearAnnouncements(state) {
+      state.announcements = [];
+    }
+  },
+  actions: {
+    async loadSitewideAnnouncements({ commit }) {
+      const response = await api.getSitewideAnnouncements();
+      commit('loadAnnouncements', { announcements: response.announcements });
+    },
+    async loadEventAnnouncements({ commit }, { eventId }) {
+      const response = await api.getEventAnnouncements(eventId);
+      commit('loadAnnouncements', { announcements: response.announcements });
+    },
+    async deleteAnnouncement({ commit }, { announcementId }) {
+      await api.deleteAnnouncement(announcementId);
+      commit('removeAnnouncement', { announcementId })
+    }
+  },
+};

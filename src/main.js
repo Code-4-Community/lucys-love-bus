b674a7a4640d0449860b7d938b0eadbd4a107be3
Jack Blanc <jblanc222@gmail.com>
2020-10-26 19:55:21 -0400

    Install Sentry (#122)
    * install sentry
    
    * move to env

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+import { Integrations } from '@sentry/tracing';
 import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
+
+Sentry.init({
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [
+    new VueIntegration({
+      Vue,
+      tracing: true,
+    }),
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 /* eslint-disable no-new */
 new Vue({
